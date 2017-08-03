130a639e3a51abc73413f3ccf3cba3043a75a85a
joetron <joetron@hotmail.com>
2017-08-03 15:37:27 -0700

    Changed back to hash history for github pages.

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import registerServiceWorker from './registerServiceWorker';
 import { ConnectedRouter, routerMiddleware } from 'react-router-redux';
-import createHistory from 'history/createBrowserHistory';
+import createHistory from 'history/createHashHistory';
 import PickerBallApp from './containers/PickerBallApp';
 import rootReducer from './reducers';
 import './index.css';
